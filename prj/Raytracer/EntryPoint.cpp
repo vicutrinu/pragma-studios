b03d99be67216a2f31e122b3ca0fd877e70120a6
vicutrinu <vicutrinu@gmail.com>
2009-12-30 01:08:50 +0000

    git-svn-id: http://pragma-studios.googlecode.com/svn/trunk@46 a041a8fe-e45c-11de-8d18-5b59c22968bc

--- prj/Raytracer/EntryPoint.cpp
+++ prj/Raytracer/EntryPoint.cpp
@@ -15,7 +15,96 @@
 
 using namespace pragma;
 
-#define IMAGE_SIZE 512
+#define IMAGE_SIZE 1024
+
+Point lLigth(0,54,0);
+
+namespace pragma
+{
+	class Multisample
+	{
+	public:
+		Multisample(size_t aSamplesPerPixel)
+			: mSamplesPerPixel(aSamplesPerPixel)
+		{
+			mValues.reserve(mSamplesPerPixel);
+			size_t lWidth = (size_t)pragma::sqrt<float>((float)mSamplesPerPixel);
+			size_t lHeigth = mSamplesPerPixel / lWidth;
+			vector2f lOffset( 1.f / (lWidth+1), 1.f / (lHeigth+1));
+			vector2f lSample(0,0);
+			for(size_t i = 0; i < lHeigth; ++i)
+			{
+				lSample.y+= lOffset.y;
+				for(size_t j = 0; j < lWidth; ++j)
+				{
+					lSample.x+= lOffset.x;
+					mValues.push_back(lSample);										
+				}
+			}
+		}
+
+		size_t GetSamplesPerPixel() const { return mSamplesPerPixel; }
+		const vector2f GetSampleOffset(size_t aIndex) const
+		{
+			return mValues[aIndex];
+			
+		}
+
+	private:
+		size_t mSamplesPerPixel;
+		std::vector<vector2f> mValues;
+	};
+
+	class Raytracer
+	{
+	public:
+		Raytracer(const Mesh& aMesh, const MaterialLibrary& aMaterialLibrary)
+			: mMesh(aMesh)
+			, mCollisionMap(mMesh)
+			, mMaterialLibrary(aMaterialLibrary)
+		{
+			mCollisionMap.ResetStats();			
+		}
+
+		Color TraceCameraRay(const Point& aPosition, const Vector& aDirection, Real aLength)
+		{
+			Color lRetVal(0,0,0);
+			MeshCollision::TResult lResult;
+
+			if( mCollisionMap.IntersectRay( aPosition, aDirection, aLength, lResult ) )
+			{
+				// Collision, trace Lighting ray
+				size_t lCount;
+				const Mesh::TTriangle& lTri = mMesh.GetTriangles(lCount)[lResult.mTriangleIndex];
+
+				Vector lToLigth = Normalize(lLigth - lResult.mPosition);
+				Vector lCollisionPoint = lResult.mPosition + lToLigth * (4.f * math::type_traits<Real>::epsilon);
+				bool lShadowRayCollision = mCollisionMap.IntersectRay( lCollisionPoint, lLigth );
+				if( !lShadowRayCollision ) 
+				{
+					Vector lNormal;
+					ComputeFromBarycentric( mMesh.GetVertexNormal(lTri.mVertexNormal[0])
+										  , mMesh.GetVertexNormal(lTri.mVertexNormal[1])
+										  , mMesh.GetVertexNormal(lTri.mVertexNormal[2])
+										  , lResult.mBarycentric
+										  , lNormal );
+					Real lDiffuse = DotProduct(lNormal, Normalize(lLigth-lCollisionPoint));
+					if(lDiffuse > 0)
+					{
+						lRetVal = mMaterialLibrary.GetMaterial(lTri.mMaterial).GetdiffuseColor() * lDiffuse;
+					}
+				}
+			}
+
+			return lRetVal;
+		}
+
+	private:
+		const Mesh& mMesh;
+		const MaterialLibrary& mMaterialLibrary;
+		MeshCollision mCollisionMap;
+	};
+}
 
 int main(int argc, char* argv[])
 {
@@ -36,86 +125,53 @@ int main(int argc, char* argv[])
 	lTransform = Inverse(lTransform);
 
 	lStart = GetTimeMilliseconds();
-	MeshCollision lCollisionMap(lMesh);
 	lEnd = GetTimeMilliseconds();
 	printf("Created KdTree in %d milliseconds\n", lEnd - lStart);
-
-	Point lLigth(0,54,0);
 
 	size_t lVertexCount;
 	const Point* lVertexs = lMesh.GetVertexs(lVertexCount);
 
-	Real* lImage = new Real[IMAGE_SIZE*IMAGE_SIZE*3];
-	Real* lPtr = lImage;
+	Color* lImage = new Color[IMAGE_SIZE*IMAGE_SIZE];
+	Color* lPtr = lImage;
 
 	lStart = GetTimeMilliseconds();
-	lCollisionMap.ResetStats();
+
+	Multisample lMultisample(1);
+
+	Raytracer lRaytracer(lMesh, lMaterialLibrary);
+
+	// Scanline renderer
 	for(size_t i = 0; i < IMAGE_SIZE; ++i)
 	{
 		for(size_t j = 0; j < IMAGE_SIZE; ++j)
 		{
-			vector4f lRay(((j*2.f)/(IMAGE_SIZE-1))-1, 1-((i*2.f)/(IMAGE_SIZE-1)), 1, 1);
-			vector4f lRes = TransformPoint(lTransform, lRay);
-			lRes = lRes * (1.f / lRes.w);
-
-			Vector lDir(lRes.x, lRes.y, lRes.z);
-			lDir = Normalize(lDir);
-
-			MeshCollision::TResult lResult;
-
-			if( lCollisionMap.IntersectRay( lCameraPos, lDir, 1000, lResult ) )
+			*lPtr = Color(0,0,0);
+			// Multiple samples per pixel
+			for(size_t s = 0; s < lMultisample.GetSamplesPerPixel(); ++s)
 			{
-				size_t lCount;
-				const Mesh::TTriangle& lTri = lMesh.GetTriangles(lCount)[lResult.mTriangleIndex];
-				
-				Vector lCollisionPoint = lResult.mPosition + Normalize<Real>(lResult.mPosition-lLigth) * (4.f * math::type_traits<Real>::epsilon);
-				if(lCollisionMap.IntersectRay( lCollisionPoint, lLigth ))
-				{
-					lPtr[0] = 0; lPtr[1] = 0; lPtr[2] = 0;
-				}
-				else
-				{
-					Vector lNormal;
-					ComputeFromBarycentric( lMesh.GetVertexNormal(lTri.mVertexNormal[0])
-										  , lMesh.GetVertexNormal(lTri.mVertexNormal[1])
-										  , lMesh.GetVertexNormal(lTri.mVertexNormal[2])
-										  , lResult.mBarycentric
-										  , lNormal );
-					//lNormal = lMesh.GetTriangleNormal( lResult.mTriangleIndex );
-					/*
-					Point lA, lB, lC;
-					lMesh.GetTriangleVertexs(lResult.mTriangleIndex, lA, lB, lC);
-					lNormal = Normalize( CrossProduct( lB - lA, lC - lA ) );
-					*/
-					Real lDiffuse = DotProduct(lNormal, Normalize(lLigth-lCollisionPoint));
-					Color lColor = lMaterialLibrary.GetMaterial(lTri.mMaterial).GetdiffuseColor();
-					lColor = lColor * lDiffuse;
-					if(lDiffuse > 0)
-					{
-						lPtr[0] = lColor.x;
-						lPtr[1] = lColor.y;
-						lPtr[2] = lColor.z;
-					}
-					else
-					{
-						lPtr[0] = 0; lPtr[1] = 0; lPtr[2] = 0;
-					}
-				}
+				vector2f lOffset = lMultisample.GetSampleOffset(s);
+				vector4f lRay((((lOffset.x+j)*2.f)/(IMAGE_SIZE-1))-1, 1-(((lOffset.y+i)*2.f)/(IMAGE_SIZE-1)), 1, 1);
+				vector4f lRes = TransformPoint(lTransform, lRay);
+				lRes = lRes * (1.f / lRes.w);
+
+				Vector lDir(lRes.x, lRes.y, lRes.z);
+				lDir = Normalize(lDir);
+
+				*lPtr = *lPtr + lRaytracer.TraceCameraRay( lCameraPos, lDir, 1000 );
 			}
-			else
-			{
-				lPtr[0] = 0; lPtr[1] = 0; lPtr[2] = 0;
-			}
-			lPtr+= 3;
+			*lPtr = *lPtr / lMultisample.GetSamplesPerPixel();
+			lPtr++;
 		}
 	}
 	lEnd = GetTimeMilliseconds();
-	printf("Image rendered in %d milliseconds. Traced %d rays\n", lEnd - lStart, lCollisionMap.TracedRays());
+	//printf("Image rendered in %d milliseconds. Traced %d rays\n", lEnd - lStart, lCollisionMap.TracedRays());
 
 	uint8* lRAWImage = new uint8[IMAGE_SIZE*IMAGE_SIZE*3];
-	for(size_t i = 0; i < IMAGE_SIZE*IMAGE_SIZE*3; ++i)
+	for(size_t i = 0; i < IMAGE_SIZE*IMAGE_SIZE; ++i)
 	{
-		lRAWImage[i] = uint8(lImage[i] * 255.f);
+		lRAWImage[i*3+0] = uint8(lImage[i].x * 255.f);
+		lRAWImage[i*3+1] = uint8(lImage[i].y * 255.f);
+		lRAWImage[i*3+2] = uint8(lImage[i].z * 255.f);
 	}
 	FILE* handle = fopen("out.raw","wb");
 	fwrite(lRAWImage, IMAGE_SIZE*IMAGE_SIZE, 3, handle);
--- src/pragma/graphics/types.h
+++ src/pragma/graphics/types.h
@@ -8,6 +8,6 @@ namespace pragma
 	typedef vector3<double> Point;
 	typedef vector3<double> Vector;
 	typedef vector2<double> Vector2;
-	typedef double    Real;
+	typedef double			Real;
 	typedef vector3<double> Color;
 }
