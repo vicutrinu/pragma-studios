f21cb27a6b76bee7f08ea37e9b7bb96d1667461a
vicutrinu <vicutrinu@gmail.com>
2011-09-11 16:10:23 +0200

    BSP tree

---
+++ src/pragma/graphics/BSPTree.cpp
@@ -0,0 +1,9 @@
+//
+//  BSPTree.cpp
+//  pragma
+//
+//  Created by Victor Jose Soria on 07/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ src/pragma/graphics/BSPTree.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  pragma
+//
+//  Created by Victor Jose Soria on 07/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef pragma_Header_h
+#define pragma_Header_h
+
+
+
+#endif
